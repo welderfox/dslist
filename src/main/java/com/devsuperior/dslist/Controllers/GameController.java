37536e09f95b685f75fd6bd5ff825cbeb2efe195
welder <welderfox@hotmail.com>
2025-01-22 23:30:12 -0300

    EndPoint Game by Id

--- src/main/java/com/devsuperior/dslist/Controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/Controllers/GameController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
@@ -19,16 +20,15 @@ public class GameController {
 	@Autowired
 	private GameService gameService;	
 
-	/*@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{id}")
 	public GameDTO findById(@PathVariable Long id) {
-		GameDTO result = gameService.findById(id);
-		return result;
-	}*/
+		return gameService.findById(id);
+
+	}
 
 	@GetMapping
 	public List<GameMinDTO> findAll() {
-		List<GameMinDTO> result = gameService.findAll();
-		return result;
+		return gameService.findAll();
 	}
 	
 	/*@GetMapping
